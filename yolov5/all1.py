871f82a37fee0927af25e8ad94a2206b791ca725
MyJetson <smdrone1@gmail.com>
2023-03-09 01:53:15 +0100

    1 file complete

--- yolov5/all1.py
+++ yolov5/all1.py
@@ -2,8 +2,6 @@
 
 import cv2
 import depthai as dai
-
-import argparse
 import os
 import platform
 import sys
@@ -19,13 +17,63 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
-                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, non_max_suppression, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, smart_inference_mode
 
-stepSize = 0.05
+import cv2
 
+torch.no_grad()
+
+weights=ROOT / 'v5.pt' # model path or triton URL
+source=ROOT / '0' # file/dir/URL/glob/screen/0(webcam)
+data=ROOT / 'data/coco128.yaml' # dataset.yaml path
+imgsz=(416, 640) # inference size (height, width)
+conf_thres=0.25 # confidence threshold
+iou_thres=0.45 # NMS IOU threshold
+max_det=1000 # maximum detections per image
+device=0 # cuda device, i.e. 0 or 0,1,2,3 or cpu
+view_img=False # show results
+nosave=False # do not save images/videos
+classes=None # filter by class: --class 0, or --class 0 2 3
+agnostic_nms=False # class-agnostic NMS
+augment=False # augmented inference
+visualize=False # visualize features
+update=False # update all models
+project=ROOT / 'runs/detect' # save results to project/name
+name='exp' # save results to project/name
+exist_ok=False # existing project/name ok, do not increment
+line_thickness=3 # bounding box thickness (pixels)
+half=False # use FP16 half-precision inference
+dnn=False # use OpenCV DNN for ONNX inference
+vid_stride=1 # video frame-rate stride
+
+source = str(source)
+save_img = not nosave and not source.endswith('.txt')  # save inference images
+is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
+screenshot = source.lower().startswith('screen')
+if is_url and is_file:
+    source = check_file(source)  # download
+
+# Load model
+device = select_device(device)
+model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+stride, names, pt = model.stride, model.names, model.pt
+imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+# Run inference
+seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+
+# DEPTH
 newConfig = False
 
 # Create pipeline
@@ -126,10 +174,50 @@ with dai.Device(pipeline) as device:
             cv2.putText(depthFrameColor, f"X: {int(depthData.spatialCoordinates.x)} mm", (xmin + 10, ymin + 20), fontType, 0.5, 255)
             cv2.putText(depthFrameColor, f"Y: {int(depthData.spatialCoordinates.y)} mm", (xmin + 10, ymin + 35), fontType, 0.5, 255)
             cv2.putText(depthFrameColor, f"Z: {int(depthData.spatialCoordinates.z)} mm", (xmin + 10, ymin + 50), fontType, 0.5, 255)
+        
         # Show the frame
         cv2.imshow("depth", depthFrameColor)
-        cv2.imshow("rgb", rgbFrame)
-
+        # cv2.imshow("rgb", rgbFrame)
+        
+        s =""
+        im = rgbFrame.reshape(1, 3, 416, 640)
+        
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+    
+        # Inference
+        with dt[1]:
+            pred = model(im, augment=augment)
+    
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+    
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            if webcam:
+                s += f'{i}: '
+    
+            s += '%gx%g ' % im.shape[2:]  # print string
+            gn = torch.tensor((800, 1280, 3))[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], (800, 1280, 3)).round()
+    
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                for *xyxy, conf, cls in reversed(det):
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    print(cls, *xywh, conf)
+        
+	
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
