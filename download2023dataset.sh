a44545a483f107a11a08b5fd4fef93becb69afef
Sean Mabli <smdrone1@gmail.com>
2023-07-09 13:47:08 -0400

    remove unziping and ziping on server

--- download2023dataset.sh
+++ download2023dataset.sh
@@ -1,3 +1,2 @@
-ssh team4169@api.seanmabli.com "cd datasets; zip -r frc2023dataset.zip frc2023dataset"
 scp team4169@api.seanmabli.com:~/datasets/frc2023dataset.zip .
 unzip frc2023dataset.zip
--- upload2023dataset.sh
+++ upload2023dataset.sh
@@ -3,4 +3,4 @@ read varname
 zip -r frc2023dataset.zip frc2023dataset
 scp frc2023dataset.zip team4169@api.seanmabli.com:~/datasets/frc2023dataset.zip
 rm frc2023dataset.zip
-ssh team4169@api.seanmabli.com "cd datasets; rm -fr frc2023dataset/; unzip -o frc2023dataset.zip -d .; git add -A; git commit -m 'update dataset - $varname'"
+ssh team4169@api.seanmabli.com "cd datasets; git add -A; git commit -m 'update dataset - $varname'"
