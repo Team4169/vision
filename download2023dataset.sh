c8b46d38a80024710ee2febc2ab57727f23e03e0
Sean Mabli <smdrone1@gmail.com>
2023-07-08 23:52:39 -0400

    new download and upload system

--- download2023dataset.sh
+++ download2023dataset.sh
@@ -1,2 +1,3 @@
+ssh team4169@api.seanmabli.com "cd datasets; zip -r frc2023dataset.zip frc2023dataset"
 scp team4169@api.seanmabli.com:~/datasets/frc2023dataset.zip .
 unzip frc2023dataset.zip
--- upload2023dataset.sh
+++ upload2023dataset.sh
@@ -1,4 +1,6 @@
+echo GitHub Username:
+read varname
 zip -r frc2023dataset.zip frc2023dataset
 scp frc2023dataset.zip team4169@api.seanmabli.com:~/datasets/frc2023dataset.zip
 rm frc2023dataset.zip
-ssh team4169@api.seanmabli.com "unzip -o datasets/frc2023dataset.zip -d datasets/frc2023dataset; git add -A; git commit -m 'Update dataset'"
+ssh team4169@api.seanmabli.com "cd datasets; rm -fr frc2023dataset/; unzip -o frc2023dataset.zip -d .; git add -A; git commit -m 'update dataset - $varname'"
