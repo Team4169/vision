5616126d12157d728168b5d1feffe5cc68811899
seanmabli <seanmabli4@gmail.com>
2023-10-10 23:28:30 +0200

    reorganize

--- upload2023dataset.sh
+++
@@ -1,6 +0,0 @@
-echo GitHub Username:
-read varname
-zip -r frc2023dataset.zip frc2023dataset
-scp frc2023dataset.zip team4169@api.seanmabli.com:~/datasets/frc2023dataset.zip
-rm frc2023dataset.zip
-ssh team4169@api.seanmabli.com "cd datasets; git add -A; git commit -m 'update dataset - $varname'"
