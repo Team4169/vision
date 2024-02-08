a6b7044432dc3ffb6062f56cca6f225183db58d2
goraya25 <goraya25@lsrhs.net>
2024-02-08 22:38:03 +0100

    working yolo

--- depth/oakYolov8.py
+++ depth/oakYolov8.py
@@ -1,13 +1,26 @@
 from depthai_sdk import OakCamera, ArgsParser
+from depthai_sdk.visualize.configs import BboxStyle, TextPosition
 import argparse
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='/home/jetson/vision/depth/2023Yolov8DepthAIToolsresult/combinedyolov8.json', type=str)
+parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='/home/jetson/vision/depth/yolov8ntest1/YOLOv8nNORO.json', type=str)
 args = ArgsParser.parseArgs(parser)
 
 with OakCamera(args=args) as oak:
     color = oak.create_camera('color')
     nn = oak.create_nn(args['config'], color, nn_type='yolo', spatial=True)
-    oak.visualize(nn.out.passthrough, fps=True)
+    # print(nn.out.nn_data)
+    visualizer = oak.visualize(nn.out.passthrough, fps=True)
+    visualizer.detections(
+        color=(255, 255, 255),
+        thickness=2,
+        bbox_style=BboxStyle.RECTANGLE,
+        label_position=TextPosition.MID,
+    ).text(
+        font_color=(255, 255, 255),
+        auto_scale=True
+    ).tracking(
+        line_thickness=5
+    )
     oak.start(blocking=True)
