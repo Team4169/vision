8f6078401f459f8d5d5119d5679007869854fb87
seanmabli <seanmabli4@gmail.com>
2024-02-08 22:11:18 -0500

    print detections to terminal with depthai_sdk

--- depth/oakYolov8.py
+++ depth/oakYolov8.py
@@ -1,18 +1,22 @@
 from depthai_sdk import OakCamera, ArgsParser
 from depthai_sdk.visualize.configs import BboxStyle, TextPosition
-import argparse
+import argparse, time
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='/home/jetson/vision/depth/yolov8ntest1/YOLOv8nNORO.json', type=str)
+parser.add_argument("-conf", "--config", help="Trained YOLO json config path", default='/home/team4169/vision/depth/yolov8ntest1/YOLOv8nNORO.json', type=str)
 args = ArgsParser.parseArgs(parser)
+
+def printDetections(packet):
+    print(len(packet.detections))
+    for i in range(len(packet.detections)):
+        print(packet.detections[i].confidence)
+        print(packet.detections[i].label_str)
+        print(packet.detections[i].img_detection.spatialCoordinates.x, packet.detections[i].img_detection.spatialCoordinates.y, packet.detections[i].img_detection.spatialCoordinates.z)
 
 with OakCamera(args=args) as oak:
     color = oak.create_camera('color')
     nn = oak.create_nn(args['config'], color, nn_type='yolo', spatial=True)
-    # print(nn.out.nn_data)
-    outer = nn.out
-    visualizer = oak.visualize([nn.out.tracker, nn.out.passthrough], fps=True)
-
-    print(visualizer.get_bbox())
+    visualizer = oak.visualize(nn.out.passthrough, callback=printDetections)
+    visualizer = oak.visualize(nn.out.passthrough, fps=True)
     oak.start(blocking=True)
