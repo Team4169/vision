7496fca52bbd57349bdf4533b219438d6786f564
seanmabli <seanmabli4@gmail.com>
2024-02-15 22:38:13 +0100

    finished 3dcams

--- apriltags/server3dcams.py
+++ apriltags/server3dcams.py
@@ -1,6 +1,20 @@
-import apriltag, cv2, socket
+import apriltag, cv2, json
 import numpy as np
-from time import sleep, time
+import json
+import ntcore
+import wpilib
+
+#<Init datatables> vdef robotInit() -> None:
+
+inst = ntcore.NetworkTableInstance.getDefault()
+
+table = inst.getTable("datatable")
+
+wPub = table.getDoubleTopic("w").publish()
+xPub = table.getDoubleTopic("y").publish()
+yPub = table.getDoubleTopic("x").publish()
+rPub = table.getDoubleTopic("r").publish()
+aPub = table.getDoubleTopic("a").publish()
 
 cam_props = {
 'back':{'cam_matrix': np.array([[649.12576246,0,349.34554103],[0,650.06837252,219.01641695],[0,0,1]],dtype=np.float32), 'dist': np.array([0.09962565,-0.92286434,-0.00491307,0.00470977,1.3384658], dtype = np.float32), 'offset':np.array([5.5,17,-0.5],dtype=np.float32)},
@@ -9,35 +23,19 @@ cam_props = {
 'right':{'cam_matrix': np.array([[675.54311877,0,315.7372509],[0,675.09333584,230.65457206],[0,0,1]],dtype=np.float32), 'dist': np.array([1.56483688e-01,-1.12875978e+00,4.13870402e-03,-1.00809719e-03,1.65813324e+00], dtype = np.float32), 'offset':np.array([-7,20.5,-1.5],dtype=np.float32)}}
 
 # <Assign correct params to correct cams> v
-camidnames = ['x','x','x','x']
+camidnames = ['x','x']
 
-# Server configuration
-
-
-host = '0.0.0.0'  # Listen on all available interfaces
-port = 12345       # Choose a port number
-
-# Create a socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to a specific address and port
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((host, port))
-
-# Listen for incoming connections (max queue size is set to 1)
-server_socket.listen(1)
-
-print(f"Ready to Send Data on {host}:{port}")
-
-# Accept a connection
-client_socket, client_address = server_socket.accept()
-print(f"Connection from {client_address}")
-
-def SendData(data):
-    client_socket.send(str(data).encode('utf-8'))
+# Get Apritag locations
+file_path = "/home/jetson/vision/apriltags/maps/computerLab.fmap"
+with open(file_path, 'r') as file:
+    data = json.load(file)
+coordinates = {}
+for apriltag_ in data["fiducials"]:
+    coordinates[apriltag_["id"]] = [apriltag_["transform"][3], apriltag_["transform"][7]]
 
 def IDCams():
     for i in range(len(all_caps)):
+        camidnames[i] = 'front'
         image = all_caps[i].read()[1]
         cv2.imshow('cap ' + str(i), image)
         cv2.waitKey(200)
@@ -51,25 +49,29 @@ def IDCams():
         elif camname == 'b':
             camname = 'back'
         else:
-            raise ValueError('You failed. Use l,r,f, or b.')
+            raise ValueError('Error. Use l, r, f, or b.')
         camidnames[i] = camname
         cv2.destroyAllWindows()
 # </Assign correct params to correct cams> ^
 
-options = apriltag.DetectorOptions(families='tag16h5',
-                                   border=2,
+options = apriltag.DetectorOptions(families='tag36h11',
+                                   border=1,
                                    nthreads=4,
-                                   quad_decimate=1.0,
+                                   quad_decimate=0.0,
                                    quad_blur=0.0,
                                    refine_edges=True,
                                    refine_decode=False,
                                    refine_pose=False,
                                    debug=True,
-                                   quad_contours=True)
+                                   quad_contours=False)
+
+detector = apriltag.Detector(options) 
+
 def findtags(cap, name):
     
-    detector = apriltag.Detector(options) 
     image = cap.read()[1]
+    
+    #image = cv2.resize(image, (640,480)) ONLY USE IF using capn.set()
     
     # <get params> v
     camera_matrix = cam_props[camidnames[int(name)]]['cam_matrix']
@@ -94,10 +96,8 @@ def findtags(cap, name):
     results = detector.detect(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
 
     # loop over the AprilTag detection results
-    tagsToBeSent=[]
+    posList = []
     for r in results:
-        
-        
 
         # extract the bounding box (x, y)-coordinates for the AprilTag
         # and convert each of the (x, y)-coordinate pairs to integers
@@ -110,68 +110,87 @@ def findtags(cap, name):
         # draw the bounding box of the AprilTag detection
         cv2.line(image, iptA, iptB, (0, 255, 0), 2)
         cv2.line(image, iptB, iptC, (0, 255, 0), 2)
-        cv2.line(image, iptC, iptD, (0, 255, 0), 2)
         cv2.line(image, iptD, iptA, (0, 255, 0), 2)
         
         # draw the center (x, y)-coordinates of the AprilTag
         icenter = (int(r.center[0]), int(r.center[1]))
         cv2.circle(image, icenter, 5, (0, 0, 255), -1)
-        u = 3 * scalefac ** 2
-        object_points = np.array([[-u,-u,0],[u,-u,0],[u,u,0],[-u,u,0],[0,0,0]], dtype=np.float32)
+
+        U = .085725 # meters, use 3.375 for inches
+
+        object_points = np.array([[-U,-U,0],[U,-U,0],[U,U,0],[-U,U,0],[0,0,0]], dtype=np.float32)
         
         image_points = np.array([ptA,ptB,ptC,ptD,r.center], dtype=np.float32)
         
         
         _, rvec, tvec = cv2.solvePnP(object_points, image_points, camera_matrix, distortion_coefficients)
         
-        # <rotate>
+        # <rotate> v
         if camidnames[int(name)] == 'back':
             tvec = np.array([-tvec[0],tvec[1],-tvec[2]], dtype=np.float32)
         elif camidnames[int(name)] == 'left':
             tvec = np.array([-tvec[2],tvec[1],tvec[0]], dtype=np.float32)
         elif camidnames[int(name)] == 'right':
             tvec = np.array([tvec[2],tvec[1],-tvec[0]], dtype=np.float32)
-        # </rotate>
+        # </rotate> ^
         
         for i, offset_num in enumerate(origin_offset):
             tvec[i] -= offset_num
 
-        tvec += origin_offset
+        
+        if (r.tag_id in coordinates):
+            posList.append(
+                ([coordinates[r.tag_id][0] - tvec[0], coordinates[r.tag_id][1] - tvec[2]])
+            )
+
+        '''
         if tvec is not None:
-            tagsToBeSent.append({'id':r.tag_id , 'camName':camidnames[int(name)] , 'rvec':rvec.tolist() , 'tvec':tvec.tolist() , 'timeDetected':time()})
+            print("tag found on cam", name, "\nrvec:",rvec,"\ntvec:",tvec,"\npythag dist:",(float(tvec[0])**2+float(tvec[1])**2+float(tvec[2])**2)**.5)
         else:
-            pass #bad tvec
+            print("bad tvec")
+        '''
         cv2.putText(image, str(r.tag_id), (icenter[0], icenter[1] - 15),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        
+    
     # show the output image after AprilTag detection
     cv2.imshow(name, image)
-    return tagsToBeSent
+    return posList
 
-#cap0 = cv2.VideoCapture(0)
-#cap1 = cv2.VideoCapture(1)
-cap2 = cv2.VideoCapture(1)
-cap3 = cv2.VideoCapture(2)
-#all_caps = [cap0, cap1, cap2, cap3]
-all_caps = [cap2, cap3]
-scalefac = 1 # Max range = 13ft * scalefac
-for capn in all_caps:
-    capn.set(3, 480 * scalefac)
-    capn.set(4, 640 * scalefac)
-    capn.set(5, 12) #fps
+cap0 = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(1)
+all_caps = [cap0, cap1]
+#scalefac = 0.5
+#for capn in all_caps:
+#    capn.set(3, 480 * scalefac)
+#    capn.set(4, 640 * scalefac)
+#    capn.set(5, 2) #fps
 
 
 IDCams()
 
 while True:
     try:
-        tagsToBeSent = []
+        fullPosList = []
+        fullPosList.extend(findtags(cap0,'0'))
+        fullPosList.extend(findtags(cap1,'1'))
+        totalPos=[0,0]
+        weight=0
+        for i in fullPosList:
+            if len(i) == 0: continue
+            weight+=1
+            totalPos[0]+=i[0][0]
+            totalPos[1]+=i[1][0]
+        if weight > 0:        
+            totalPos[0] /= weight
+            totalPos[1] /= weight
+        else:
+            totalPos[0] = 0
+            totalPos[1] = 0
+        dataToSend = {'weight':weight, 'posx':totalPos[0], 'posy':totalPos[1]}
+        print(json.dumps(dataToSend))
+        aPub.set(dataToSend)
         
-        tagsToBeSent.extend( findtags(cap2,'0') )
-        tagsToBeSent.extend( findtags(cap3,'1') )
-        
-        SendData({'apriltags':tagsToBeSent})
-        print(tagsToBeSent)
+        # On roborio, weight average of fullPosList(jetson1) and fullPosList(jetson2) based on len(fullPosList)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -179,11 +198,7 @@ while True:
     except(KeyboardInterrupt):
         cap0.release()
         cap1.release()
-        cap2.release()
-        cap3.release()
-
+        
         print("/nCams Off. Program ended.")
         cv2.destroyAllWindows()
-        client_socket.close()
-        server_socket.close()
         break
