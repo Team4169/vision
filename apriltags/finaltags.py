e3e2fb501cc01fa696c45e1f88a768eb7b80f9cd
seanmabli <seanmabli4@gmail.com>
2024-03-06 21:32:07 +0100

    finaltags

--- apriltags/finaltags.py
+++ apriltags/finaltags.py
@@ -1,14 +1,16 @@
-import apriltag, cv2, ntcore, subprocess
+import apriltag, cv2, subprocess
 from math import sin, cos, atan2, pi
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from networktables import NetworkTables
+import logging
 
 cam_props = {
-'back':{'cam_matrix': np.array([[649.12576246,0,349.34554103],[0,650.06837252,219.01641695],[0,0,1]],dtype=np.float32), 'dist': np.array([0.09962565,-0.92286434,-0.00491307,0.00470977,1.3384658], dtype = np.float32), 'offset':np.array([0,0,0],dtype=np.float32)},
-'front':{'cam_matrix': np.array([[677.79747434,0,327.64289497],[0,677.8796147,227.7857478],[0,0,1]],dtype=np.float32), 'dist': np.array([ 8.96320625e-02,-8.66510276e-01,-7.79278879e-04,6.59458019e-03,1.81798852e+00], dtype = np.float32), 'offset':np.array([0,0,0],dtype=np.float32)},
-'left':{'cam_matrix': np.array([[667.59437965,0,325.05798259],[0,667.62500105,222.46972227],[0,0,1]],dtype=np.float32), 'dist': np.array([[2.05714549e-01,-1.63695216e+00,1.35826526e-03,-9.93778299e-04,3.32154871e+00]], dtype = np.float32), 'offset':np.array([0,0,0],dtype=np.float32)},
-'right':{'cam_matrix': np.array([[675.54311877,0,315.7372509],[0,675.09333584,230.65457206],[0,0,1]],dtype=np.float32), 'dist': np.array([1.56483688e-01,-1.12875978e+00,4.13870402e-03,-1.00809719e-03,1.65813324e+00], dtype = np.float32), 'offset':np.array([0,0,0],dtype=np.float32)}}
+'back':{'cam_matrix': np.array([[659.5522522254913, 0.0, 342.14593411596394], [0.0, 660.0855257028237, 233.07985632799412], [0.0, 0.0, 1.0]],dtype=np.float32), 'dist': np.array([[0.18218171362352173, -1.3943575501329653, -0.0034890991822150033, -0.003111058479543986, 2.4948141925852796]], dtype = np.float32), 'offset':np.array([0.000,0.000,0.000],dtype=np.float32)},
+'front':{'cam_matrix': np.array([[650.6665701168481, 0.0, 308.11247568203765], [0.0, 649.267759423238, 230.2397074540069], [0.0, 0.0, 1.0]],dtype=np.float32), 'dist': np.array([[0.142925049930884, -1.1502926269495592, -0.0019150557540761415, -0.00328202292619461, 1.8141065950524837]], dtype = np.float32), 'offset':np.array([0.000,0.000,0.000],dtype=np.float32)},
+'left':{'cam_matrix': np.array([[668.2138474014353, 0.0, 332.83301545896086], [0.0, 666.4860881212383, 214.33779667521517], [0.0, 0.0, 1.0]],dtype=np.float32), 'dist': np.array([[0.22224705297101408, -1.7549821808892665, -0.005523738126667523, 0.0051301529546101616, 3.4133532108023994]], dtype = np.float32), 'offset':np.array([0.000,0.000,0.000],dtype=np.float32)},
+'right':{'cam_matrix': np.array([[660.6703723058181, 0.0, 321.2980455248988], [0.0, 658.6516133373474, 218.49261248405028], [0.0, 0.0, 1.0]],dtype=np.float32), 'dist': np.array([[0.18802634354539693, -1.5669527368643557, -0.0006972309753818612, -0.0018548904430247361, 3.04483663171066]], dtype = np.float32), 'offset':np.array([0.000,0.000,0.000],dtype=np.float32)}}}
 
 # Enviornment Initialization
 # I used this, but the fmap could work as well.
@@ -85,7 +87,7 @@ def findtags(cap, name):
         icenter = (int(r.center[0]), int(r.center[1]))
         cv2.circle(image, icenter, 5, (0, 0, 255), -1)
 
-        U = 0.0857 #In meters, use 3.375 of you want inches.
+        U = 0.085725 #In meters, use 3.375 of you want inches.
 
         object_points = np.array([[-U,-U,0],[U,-U,0],[U,U,0],[-U,U,0],[0,0,0]], dtype=np.float32)
         
@@ -155,14 +157,22 @@ front_cap = cv2.VideoCapture(int(cam_mapping["2.1"]))
 right_cap = cv2.VideoCapture(int(cam_mapping["2.2"]))
 
 # <Init NetworkTables> v
-inst = ntcore.NetworkTableInstance.getDefault()
 
-table = inst.getTable("datatable")
+logging.basicConfig(level=logging.DEBUG)
 
+NetworkTables.initialize()
+sd = NetworkTables.getTable("SmartDashboard")
+
+#inst = ntcore.NetworkTableInstance.getDefault()
+
+#table = inst.getTable("datatable")
+
+"""
 wPub = table.getDoubleTopic("w1").publish()
 xPub = table.getDoubleTopic("y1").publish()
 yPub = table.getDoubleTopic("x1").publish()
 rPub = table.getDoubleTopic("r1").publish()
+"""
 # <Init NetworkTables> ^
 
 # Init plot
@@ -181,12 +191,16 @@ while True:
             
             avg_pos = [sum(coord[0] for coord in fullPosList) / len(fullPosList), sum(coord[1] for coord in fullPosList) / len(fullPosList)]
             avg_rot = math.atan2(sum(math.sin(angle) for angle in fullRotList) / len(fullRotList), sum(math.cos(angle) for angle in fullRotList) / len(fullRotList)) % (2 * math.pi)
-            
+            """
             wPub.set(len(avg_pos), ntcore._now())
             xPub.set(avg_pos[0], ntcore._now())
             yPub.set(avg_pos[1], ntcore._now())
             rPub.set(avg_rot, ntcore._now())
-        
+            """
+            sd.putNumber("w1", len(avg_pos))
+            sd.putNumber("x1", avg_pos[0])
+            sd.putNumber("y1", avg_pos[1])
+            sd.putNumber("r1", avg_rot)
         # <Draw Code with matplotlib> v
         if True: #DRAW
             # Clear previous robots from the plot
