6254c848f7dacea8ea13de94f3f96a8626c5e232
seanmabli <seanmabli4@gmail.com>
2024-03-07 21:28:41 +0100

    fixed calibration

--- calibration/getImages.py
+++ calibration/getImages.py
@@ -1,32 +1,27 @@
 import cv2
 from time import time
- 
-<<<<<<< HEAD
 
-starttime = time() 
-
-=======
->>>>>>> 83f96681f409c7b8f4942725506b4bdd546fc148
+starttime = time()
 cap = cv2.VideoCapture(0)
- 
+
 num = 0
- 
+
 while cap.isOpened():
- 
+
     succes, img = cap.read()
- 
+
     k = cv2.waitKey(5)
- 
+
     if k == 27:
         break
     elif k == ord('s'): # wait for 's' key to save and exit
         cv2.imwrite('./images/img' + str(starttime) + str(num) + '.png', img)
         print("image saved!")
         num += 1
- 
+
     cv2.imshow('Img',img)
- 
+
 # Release and destroy all windows before termination
 cap.release()
- 
+
 cv2.destroyAllWindows()
