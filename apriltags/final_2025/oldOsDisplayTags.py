749e892b70534aafea443c9d77c6ce784c8d8cbf
Adamimoka <adamimoka@gmail.com>
2025-01-13 17:46:09 -0500

    updated display tags for combined code

--- apriltags/final_2025/oldOsDisplayTags.py
+++ apriltags/final_2025/oldOsDisplayTags.py
@@ -1,34 +1,24 @@
 import apriltag, cv2, subprocess
 from math import sin, cos, atan2, pi
 import numpy as np
+import ntcore
 import pickle
+from getmac import get_mac_address
 from time import time
-from dotenv import load_dotenv
-from os import getenv
-import sys
 
-# <Intialize enviornment variables> v
-load_dotenv()
-
-ENABLE_NETWORK_TABLES = int(getenv("ENABLE_NETWORK_TABLES"))
-NEW_OS = int(getenv("NEW_OS"))
-JETSON_ID = getenv("JETSON_ID")
-# </Intialize enviornment variables> ^
-
-if ENABLE_NETWORK_TABLES:
-    import ntcore
+enable_network_tables = False
 
 detector = apriltag.Detector(
-    apriltag.DetectorOptions(families='tag36h11',
-                             border=1,
-                             nthreads=4,
-                             quad_decimate=0.0,
-                             quad_blur=0.0,
-                             refine_edges=True,
-                             refine_decode=False,
-                             refine_pose=False,
-                             debug=True, # change maybe
-                             quad_contours=False))
+apriltag.DetectorOptions(families='tag36h11',
+                         border=1,
+                         nthreads=4,
+                         quad_decimate=0.0,
+                         quad_blur=0.0,
+                         refine_edges=True,
+                         refine_decode=False,
+                         refine_pose=False,
+                         debug=True,
+                         quad_contours=False))
 
 def findtags(cap, name):
 
@@ -79,12 +69,12 @@ def findtags(cap, name):
         tag_size = .08255 # Distance in meters from the middle of the tag to the end of the black part of the tag. Also equal to half the side length of the black part of the april tag. Change it if the tag size changes. 
         object_points = np.array([[-tag_size, -tag_size, 0], [tag_size, -tag_size, 0], [tag_size, tag_size, 0], [-tag_size, tag_size, 0], [0, 0, 0]], dtype=np.float32)
 
-        image_points = np.array([r.corners[0], r.corners[1], r.corners[2], r.corners[3], r.center], dtype=np.float32)
+        image_points = np.array([ptA, ptB, ptC, ptD, r.center], dtype=np.float32)
 
         _, rvec, tvec = cv2.solvePnP(object_points, image_points, camera_matrix, distortion_coefficients)
+
         for i, offset_num in enumerate(origin_offset):
             tvec[i] += offset_num
-
         # <Rotate Code> v
         # Based on the tag that we see, and which camera sees it, do math to find out where the robot must be to see that tag in that relative position and orientation.
         c=cos(seen_tag['Z-Rotation'] + rvec[1] + pi/2);s=sin(seen_tag['Z-Rotation'] + rvec[1] + pi/2)
@@ -103,7 +93,7 @@ def findtags(cap, name):
         position = [seen_tag['X'] - tvec[0], seen_tag['Y'] - tvec[2]]
         angle = float((seen_tag['Z-Rotation'] + rvec[1] - pi)[0])
         # </Rotate Code> ^
-        
+
         posList.append(position)
         rotList.append(angle)
 
@@ -114,98 +104,82 @@ def findtags(cap, name):
     cv2.imshow(name, image)
     return posList, rotList
 
+def getJetson():
+    MACdict = {"48:b0:2d:c1:63:9c" : 1, "48:b0:2d:ec:31:82" : 2, "48:b0:2d:c1:63:9b" : 1}
+    return MACdict.get(get_mac_address(), None) # Returns None if invalid Jetson ID
+
 def parse_v4l2_devices(output):
     mappings = {}
-    if NEW_OS:
-        lines = str(output).split('\\n\\n')
-        for line in lines:
-            if 'usb-70090000.xusb-' in line and '/dev/video' in line:
-                mappings[line.split('usb-70090000.xusb-')[1][0:3]] = int(line.split('/dev/video')[1])
-    else:
-        lines = output.split('\n')
-        current_device = None
-        for line in lines:
-            if line.strip().endswith(':'):
-                current_device = line.strip()[:-1]
-            elif '/dev/video' in line:
-                video_index = line.strip().split('/')[-1]
-                if current_device:
-                    mappings[current_device[-4:-1]] = int(video_index[-1])
+    lines = output.split('\n')
+    current_device = None
+    for line in lines:
+        if line.strip().endswith(':'):
+            current_device = line.strip()[:-1]
+        elif '/dev/video' in line:
+            video_index = line.strip().split('/')[-1]
+            if current_device:
+                mappings[current_device[-4:-1]] = int(video_index[-1])
     return mappings
 
-def get_v4l2_devices():
+def get_v4l2_device_mapping():
     try:
-        if NEW_OS:
-            output = str(subprocess.check_output(['v4l2-ctl', '--list-devices']))[2:-1]
-        else:
-            output = subprocess.check_output(['v4l2-ctl', '--list-devices'], text=True)
+        output = subprocess.check_output(['v4l2-ctl', '--list-devices'], text=True)
         return parse_v4l2_devices(output)
     except subprocess.CalledProcessError as e:
         print("Error occurred")
         return parse_v4l2_devices(e.output)
 
+jetsonID = getJetson()
 cam_0_name = ''
 cam_1_name = ''
 # Initialize cams with correct Jetson
-if JETSON_ID == "1":
+if jetsonID == 1:
     cam_0_name = "Front"
     cam_1_name = "Right"
-elif JETSON_ID == "2":
+elif jetsonID == 2:
     cam_0_name = "Back"
     cam_1_name = "Left"
 else:
     raise Exception('Invalid Jetson ID')
-
-cam_mapping = get_v4l2_devices()
-
+cam_mapping = get_v4l2_device_mapping()
 cam_0 = cv2.VideoCapture(int(cam_mapping["2.1"]))
 cam_1 = cv2.VideoCapture(int(cam_mapping["2.2"]))
 
 # <Init Constants> v (cam_props and field_tags)
 cam_props = {}
-repo_path = ''
-if NEW_OS:
-    repo_path = "/home/robotics4169/vision"
-else:
-    repo_path = "/home/aresuser/vision"
-
 for cam_name in {cam_0_name, cam_1_name}:
-    with open (f"{repo_path}/calibration/camConfig/camConfig{cam_name}.pkl", 'rb') as f:
+    with open (f"/home/aresuser/vision/calibration/camConfig/camConfig{cam_name}.pkl", 'rb') as f:
         f_data = pickle.load(f)
         cam_props[cam_name] = {'cam_matrix': f_data[0], 'dist': f_data[1], 'offset': f_data[2]}
 
-with open (f"{repo_path}/apriltags/maps/fieldTagsConfig_2025.pkl", 'rb') as f:
+with open (f"/home/aresuser/vision/apriltags/maps/fieldTagsConfig_2025.pkl", 'rb') as f:
     field_tags = pickle.load(f)
-    # 'field_tags' is a list of dictionaries. It contains the data for the apriltags on the 2025 field (update for future seasons).
-    # It looks looks like: [{'ID': 1, 'X': 16.7, 'Y': 0.655, 'Z': 1.49, 'Z-Rotation': 2.20, 'Y-Rotation': 0.0} ...]
-del repo_path
+    # List of dictionaries, looks like: [{'ID': 1, 'x': 16.7, 'Y': 0.655, 'Z': 1.49, 'Z-Rotation': 2.20, 'Y-Rotation': 0.0} ...]
 # <Init Constants> ^
 
 # <Init NetworkTables> v
-if ENABLE_NETWORK_TABLES:
+if enable_network_tables:
     inst = ntcore.NetworkTableInstance.getDefault()
     table = inst.getTable("SmartDashboard")
 
-    wPub = table.getDoubleTopic(f"w{JETSON_ID}").publish()
-    xPub = table.getDoubleTopic(f"x{JETSON_ID}").publish()
-    yPub = table.getDoubleTopic(f"y{JETSON_ID}").publish()
-    rPub = table.getDoubleTopic(f"r{JETSON_ID}").publish()
-
-    inst.startClient4(f"Cameras from Jetson {JETSON_ID}")
+    wPub = table.getDoubleTopic("w1").publish()
+    xPub = table.getDoubleTopic("x1").publish()
+    yPub = table.getDoubleTopic("y1").publish()
+    rPub = table.getDoubleTopic("r1").publish()
+        
+    inst.startClient4("Cameras from Jetson 1")
     inst.setServerTeam(4169)
     inst.startDSClient()
 # <Init NetworkTables> ^
 
+del getJetson, parse_v4l2_devices, get_v4l2_device_mapping # Delete these functions from memory because they are no longer needed
+
 start_time = time()
 frame_count = 0
-
-while True: # Periodic code
-    print('FPS:',frame_count/(time()-start_time))
+while True:
     frame_count += 1
-    if time()-start_time > 5:
-        start_time = time()
-        frame_count = 1
-
+    print('FPS:',frame_count/(time()-start_time))
+    
     posList0, rotList0 = findtags(cam_0, cam_0_name)
     posList1, rotList1 = findtags(cam_1, cam_1_name)
     fullPosList = posList0 + posList1
@@ -216,14 +190,12 @@ while True: # Periodic code
         avg_rot = atan2(sum(sin(angle) for angle in fullRotList) / len(fullRotList), sum(cos(angle) for angle in fullRotList) / len(fullRotList)) % (2 * pi)
 
         # Set Network table values (Weight, Xposition, Yposition, and Rotation)
-        if ENABLE_NETWORK_TABLES:
+        if enable_network_tables:
             wPub.set(len(fullPosList))
             xPub.set(avg_pos[0])
             yPub.set(avg_pos[1])
             rPub.set(avg_rot)
         else:
             print(f"w: {len(fullPosList)}\nx: {avg_pos[0]}\ny: {avg_pos[1]}\nr: {avg_rot}\n")
-
-    if cv2.waitKey(100) & 0xFF == ord('q'):
-        break
-
+        
+    cv2.waitKey(100)
